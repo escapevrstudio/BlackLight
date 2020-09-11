53367ee9b58e6e7af08d96b1cd778d10d4d1e76f
escapevrstudio <70326258+escapevrstudio@users.noreply.github.com>
2020-09-11 19:44:23 +0200

    Add files via upload

---
+++ SchwarzlichtLampe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchwarzlichtLampe : MonoBehaviour
+{
+    public Material reveal;
+    public Light light;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        reveal.SetVector("_LightPosition", light.transform.position);
+        reveal.SetVector("_LightDirection", -light.transform.forward);
+        reveal.SetFloat("_LightAngle", light.spotAngle);
+    }
+}
